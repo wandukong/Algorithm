831074094112459b4c4f71fc91cb29ec1de595ac
wandukong <seungwan37@naver.com>
2020-10-07 01:58:37 +0900

    vector - remove & unique.

--- Memo/vector.cpp
+++ Memo/vector.cpp
@@ -1,27 +1,53 @@
-#include <string>
-#include <string.h>
-#include <vector>
 #include <algorithm>
-#include <set>
-#include <queue>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+vector<int> v = {1, 2, 3, 4, 5};
+
+vector<string> gems{"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+vector<string> gems2{"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
 int main()
 {
-    vector<int> v = {1, 2, 3, 4, 5};
-
+    //***********************************************
+    // 1. 특정 위치 값 제거하기.
+    //***********************************************
     v.erase(v.begin() + 1, v.begin() + 3); // 두번째 세번째 제거  // 1 4 5
 
     for (int i = 0; i < v.size(); i++)
-    {
-        printf("%d\n", v[i]);
-    }
+        printf("%d ", v[i]);
+    printf("\n");
 
+    //***********************************************
+    // 2. 특정 위치 값 삽입하기.
+    //***********************************************
     v.insert(v.begin() + 1, 3); // 2번째 자리에 3 삽입 // 1 3 4 5
 
     for (int i = 0; i < v.size(); i++)
-    {
-        printf("%d\n", v[i]);
-    }
+        printf("%d ", v[i]);
+    printf("\n");
+
+    //***********************************************
+    // 3. 특정 값 삭제하기
+    //***********************************************
+    v.erase(remove(v.begin(), v.end(), 1), v.end()); // 값 1 제거.
+    for (int i = 0; i < v.size(); i++)
+        printf("%d ", v[i]);
+    printf("\n");
+
+    gems.erase(remove(gems.begin(), gems.end(), "DIA"), gems.end()); // 중복 값도 다 제거해줌
+    for (int i = 0; i < gems.size(); i++)
+        printf("%s ", gems[i].c_str());
+    printf("\n");
+
+    //***********************************************
+    // 4. 중복 값 삭제하기
+    //***********************************************
+
+    sort(gems2.begin(), gems2.end());
+    gems2.erase(unique(gems2.begin(), gems2.end()), gems2.end()); // 중복 값도 다 제거해줌
+    for (int i = 0; i < gems2.size(); i++)
+        printf("%s ", gems2[i].c_str());
+    printf("\n");
 }
