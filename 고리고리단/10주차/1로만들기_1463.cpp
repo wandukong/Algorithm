8e6eec82c3fdbbee0c27493fd6ce2744c7e159c5
wandukong <seungwan37@naver.com>
2020-12-29 21:01:44 +0900

    DP

--- 고리고리단/10주차/1로만들기_1463.cpp
+++ 고리고리단/10주차/1로만들기_1463.cpp
@@ -3,20 +3,24 @@
 
 using namespace std;
 
-int n;
-int dp[1000005];
+int N, dp[1000001];
 
-int main(void)
+int main()
 {
-    scanf("%d", &n);
+    scanf("%d", &N);
+    for (int i = 0; i <= N; i++)
+        dp[i] = 9999999;
     dp[1] = 0;
-    for (int i = 2; i <= n; i++)
+    dp[2] = dp[3] = 1;
+
+    for (int i = 4; i <= N; i++)
     {
-        dp[i] = dp[i - 1] + 1;
-        if (i % 2 == 0)
-            dp[i] = min(dp[i], dp[i / 2] + 1);
+        int temp = dp[i - 1] + 1;
         if (i % 3 == 0)
-            dp[i] = min(dp[i], dp[i / 3] + 1);
+            temp = min(temp, dp[i / 3] + 1);
+        if (i % 2 == 0)
+            temp = min(temp, dp[i / 2] + 1);
+        dp[i] = temp;
     }
-    printf("%d\n", dp[n]);
+    printf("%d", dp[N]);
 }
