014e267a2b3187a168fd4ecc09dc7eeb6225ba4a
wandukong <seungwan37@naver.com>
2020-12-30 03:37:43 +0900

    DP

--- 고리고리단/10주차/RGB거리_1149.cpp
+++ 고리고리단/10주차/RGB거리_1149.cpp
@@ -3,24 +3,25 @@
 
 using namespace std;
 
-int n;
-int dp[1005][3];
-int arr[1005][3];
+int N, arr[1001][1001], dp[1001][1001];
 
-int main(void) {
-	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
-		for (int j = 0; j < 3; j++)
-			scanf("%d", &arr[i][j]);
-	
-	dp[1][0] = arr[1][0];
-	dp[1][1] = arr[1][1];
-	dp[1][2] = arr[1][2];
-	for (int i = 2; i <= n; i++){
-		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-		dp[i][2] = min(dp[i - 1][1], dp[i - 1][0]) + arr[i][2];
-	}
-	printf("%d\n", min(dp[n][0], min(dp[n][1], dp[n][2])));
-	return 0;
+int main()
+{
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            scanf("%d", &arr[i][j]);
+
+    dp[0][0] = arr[0][0];
+    dp[0][1] = arr[0][1];
+    dp[0][2] = arr[0][2];
+
+    for (int i = 1; i < N; i++)
+    {
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+    }
+
+    printf("%d", min(dp[N - 1][0], min(dp[N - 1][1], dp[N - 1][2])));
 }
