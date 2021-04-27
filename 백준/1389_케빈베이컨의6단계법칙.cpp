2e13519673ba2952e6d72cfb93270943ebcdfc01
wandukong <seungwan37@naver.com>
2021-04-27 12:14:10 +0900

    [1389] 케빈 베이컨의 6단계 법칙

--- 백준/1389_케빈베이컨의6단계법칙.cpp
+++ 백준/1389_케빈베이컨의6단계법칙.cpp
@@ -22,7 +22,7 @@ int main()
         dp[a][b] = dp[b][a] = 1;
     }
     for (int k = 1; k <= N; k++)
-        for (int i = 1; i <= N; i++)
+        for (int i = 1; i <= N; i++) 
             for (int j = 1; j <= N; j++)
                 dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
 
@@ -37,3 +37,7 @@ int main()
     printf("%d", ans);
     return 0;
 }
+
+
+
+
