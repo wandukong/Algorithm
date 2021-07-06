f84394c6b1727a3ab7eed2c70d0333355100fb0a
wandukong <seungwan37@naver.com>
2021-07-06 11:55:24 +0900

    [1759] 암호 만들기

--- SDS/알고리즘기초/암호만들기_1759.cpp
+++ SDS/알고리즘기초/암호만들기_1759.cpp
@@ -10,29 +10,27 @@ vector<char> arr;
 vector<char> ans;
 map<char, int> m;
 
-void recur(int loc, int cnt)
+void recur(int loc, int cnt, int countMoeum, int countJaeum)
 {
-
-    if (cnt == N)
+    if (cnt == N && countJaeum >= 2 && countMoeum >= 1)
     {
-        int cnt1 = 0, cnt2 = 0;
         for (int i = 0; i < N; i++)
-            if (m[ans[i]])
-                cnt1++;
-            else
-                cnt2++;
-        if (cnt1 >= 1 && cnt2 >= 2)
-        {
-            for (int i = 0; i < N; i++)
-                printf("%c", ans[i]);
-            printf("\n");
-        }
+            printf("%c", ans[i]);
+        printf("\n");
     }
     for (int i = loc; i < M; i++)
     {
         ans.push_back(arr[i]);
-        recur(i + 1, cnt + 1);
+        if (m[arr[i]])
+            countMoeum++;
+        else
+            countJaeum++;
+        recur(i + 1, cnt + 1, countMoeum, countJaeum);
         ans.erase(ans.end() - 1);
+        if (m[arr[i]])
+            countMoeum--;
+        else
+            countJaeum--;
     }
 }
 
@@ -47,6 +45,6 @@ int main()
     }
     sort(arr.begin(), arr.end());
     m['a'] = m['e'] = m['i'] = m['o'] = m['u'] = true;
-    recur(0, 0);
+    recur(0, 0, 0, 0);
     return 0;
 }
