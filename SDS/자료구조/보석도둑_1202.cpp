b52c638916f418c0a2fe3c2e1947f57a0922bc76
wandukong <seungwan37@naver.com>
2021-07-08 12:06:59 +0900

    [1202] 보석 도둑(★)

---
+++ SDS/자료구조/보석도둑_1202.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int N, K;
+long long ans;
+vector<int> bag;
+vector<pair<int, int>> jwl;
+priority_queue<int> pq;
+
+int main()
+{
+    scanf("%d %d", &N, &K);
+    for (int i = 0; i < N; i++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        jwl.push_back({a, b});
+    }
+    for (int i = 0; i < K; i++)
+    {
+        int a;
+        scanf("%d", &a);
+        bag.push_back(a);
+    }
+    sort(jwl.begin(), jwl.end());
+    sort(bag.begin(), bag.end());
+
+    int jwlIdx = 0;
+    for (int i = 0; i < bag.size(); i++)
+    {
+        while (jwlIdx < N && jwl[jwlIdx].first <= bag[i])
+        {
+            pq.push(jwl[jwlIdx].second);
+            jwlIdx++;
+        }
+        if (!pq.empty())
+        {
+            ans += pq.top();
+            pq.pop();
+        }
+    }
+    printf("%d", ans);
+    return 0;
+}
