6353de3e1e4763c8245b58008a4db0e643251b1a
wandukong <seungwan37@naver.com>
2021-08-17 21:13:06 +0900

    Added week1/problem3.

---
+++ WithJava/src/homework/week1/Problem3.java
@@ -0,0 +1,28 @@
+package homework.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem3 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split(",");
+	
+		int A = Integer.parseInt(arr[0]);
+		int B = Integer.parseInt(arr[1]);
+		int C= Integer.parseInt(arr[2]);
+		int result = A*B*C;
+		int[] cnt = new int[10];
+		
+		String str = Integer.toString(result);
+		
+		for(int i = 0 ; i < str.length(); i++) 
+			cnt[str.charAt(i)-'0']++;
+		
+		for(int i = 0 ; i < 10 ; i++)
+			System.out.println(cnt[i]);
+	}
+
+}
