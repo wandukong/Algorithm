53b43efb3a5e4d3d75627e54619ec0f8a53261c3
wandukong <seungwan37@naver.com>
2021-08-22 01:41:53 +0900

    [과제3] 문제1

---
+++ WithJava/src/hyundai/ite/Problem3_1.java
@@ -0,0 +1,31 @@
+package hyundai.ite;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem3_1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split(", ");
+		
+		int sum = 0, minValue = Integer.MAX_VALUE;
+
+		for(int i = 0 ; i < arr.length ; i++) {
+			int temp = Integer.parseInt(arr[i]);
+			if((temp%2) == 1) {
+				sum+=temp;
+				minValue = Math.min(minValue, temp);
+			}
+		}
+		
+		if(minValue == Integer.MAX_VALUE)
+			System.out.println(-1);
+		else {
+			System.out.println(sum);
+			System.out.println(minValue);
+		}
+			
+	}
+}
